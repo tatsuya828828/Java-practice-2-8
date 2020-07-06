864dd52cf86c5f095a5fb19229f1671ac767e6f6
tatsuya828828 <kkjy011894@gmail.com>
2020-07-06 15:56:32 +0900

    [start]Java-practice-2-8

---
+++ src/Bank.java
@@ -0,0 +1,14 @@
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+public class Bank {
+	String name;
+	String address;
+
+	public boolean equals(Object o) {
+		return EqualsBuilder.reflectionEquals(this, o);
+	}
+
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+}
